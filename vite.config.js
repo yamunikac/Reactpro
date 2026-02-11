c9e2e5caa60cef34ac927407b4b3893a6f6108bd
Chintapanti Yamunika <chinthapantiyamunika1229@gmail.com>
2026-02-11 12:22:08 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base:"/Reactpro/",
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
 })
