24c8c00d204120bd4c9e1d7dcfcd0c0942774230
Chintapanti Yamunika <chinthapantiyamunika1229@gmail.com>
2026-02-11 10:20:49 +0530

    This is my routing project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Dashboard from "./Components/DashBoard.jsx"
+import Home from "./Components/Home.jsx";
+import Login from "./Components/Login.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
